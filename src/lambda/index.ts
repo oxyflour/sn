e9d4e21ab9e18f5444645059e1fa573c7bf711a9
oxyflour <oxyflour@hotmail.com>
2021-05-01 12:37:54 +0800

    add lambda

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -5,11 +5,7 @@ export default {
     async *stream() {
         for (let i = 0; i < 10; i ++) {
             await new Promise(resolve => setTimeout(resolve, 1000))
-            if (i === 3) {
-                throw Error('boom')
-            }
             yield i
         }
-        yield -1
     }
 }
