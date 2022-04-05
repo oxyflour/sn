9ca8ff39c00c2e914498693b0373fc6d8bf83568
oxyflour <oxyflour@hotmail.com>
2022-04-05 16:58:58 +0800

    export redux

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import web from './wrapper/web'
 import resource from './wrapper/resource'
+import * as redux from './wrapper/redux'
 
-export const wrapper = { web, resource }
+export const wrapper = { web, resource, redux }
